c33f821ca48023ada5c86a4bd2eebb488317aebc
alex escobar <231124085+alexazminime@users.noreply.github.com>
2025-09-28 21:02:08 +0000

    criacao do arquivo de testes

---
+++ arquivo de testes.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+
+void soma(){   
+
+int a = 5;
+int b = 3;
+int soma = a + b; // soma será 8
+ printf("A soma será: %d\n" ,soma);
+}
+
+void subtracao(){ 
+int a = 5;
+int b = 3;
+int subtracao = a - b; // subtracao será 2
+printf("A soma será: %d\n" ,subtracao);
+
+
+return 0;
+}
